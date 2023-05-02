fb53d8bb2f907fdf08819373ae14ea82f33c5a3d
monica-reverte <monicareverte24@gmail.com>
2023-05-02 17:00:45 +0200

    deleting

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,48 +3,7 @@ const bcrypt = require("bcrypt");
 const { uploadImg } = require ("../utils/cloudinary");
 const fs = require("fs-extra");
 
-const addNewUser = async (req, res) => {
-  const { name, email, password, confirmPassword } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
-
-    if (user) {
-      return res.status(400).json({
-        ok: false,
-        msg: "A user already exists with this email",
-      });
-    }
-
-    if (password !== confirmPassword) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Password do not match",
-      });
-    }
-
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      name,
-      email,
-      password: hash,
-    });
-
-    await newUser.save();
-
-    return res.status(201).json({
-      ok: true,
-      user: newUser._id,
-    });
-  } catch (error) {
-    return res.status(503).json({
-      ok: false,
-      msg: "Something happened",
-    });
-  }
-};
 
 const aut0Login = async (req, res) => {
 
@@ -86,46 +45,11 @@ const aut0Login = async (req, res) => {
 };
 
 
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Wrong email or password",
-      });
-    }
-
-    const userPassword = user.password;
-    const isPasswordOk = await bcrypt.compare(password, userPassword);
-
-    if (!isPasswordOk) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Wrong email or password",
-      });
-    }
-    return res.status(200).json({
-      ok: true,
-      user: user._id,
-    });
-  } catch (error) {
-    return res.status(503).json({
-      ok: false,
-      msg: "Something happened",
-    });
-  }
-};
 
 const editImage = async (req, res) => {
   console.log(req.body)
   const { userId } = req.body;
-  if (!req.params.id) {
-    return res.status(400).json({ message: "User ID is required" });
-  }
-  
+
 
   try {
     const user = await User.findOne({ _id: userId });
@@ -163,8 +87,6 @@ const editImage = async (req, res) => {
 
 
 module.exports = {
-  addNewUser,
-  loginUser,
   editImage,
   aut0Login
 };
