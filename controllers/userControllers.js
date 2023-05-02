a18791382da4e014e41ebd6bc8400ae453d27678
Isaura Martí <imarti.teixido@gmail.com>
2023-05-02 16:14:46 +0200

    changes edit img

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,52 +3,7 @@ const bcrypt = require("bcrypt");
 const { uploadImg } = require("../utils/cloudinary");
 const fs = require("fs-extra");
 
-const addNewUser = async (req, res) => {
-  const { name, email, password, confirmPassword } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-
-    if (user) {
-      return res.status(400).json({
-        ok: false,
-        msg: "A user already exists with this email",
-      });
-    }
-
-    if (password !== confirmPassword) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Password do not match",
-      });
-    }
-
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      name,
-      email,
-      password: hash,
-    });
-
-    await newUser.save();
-
-    return res.status(201).json({
-      ok: true,
-      user: newUser._id,
-    });
-  } catch (error) {
-    return res.status(503).json({
-      ok: false,
-      msg: "Something happened",
-    });
-  }
-};
-
 const aut0Login = async (req, res) => {
-  console.log(req.body.email);
-
   try {
     const user = await User.findOne({ email: req.body.email });
 
@@ -66,45 +21,17 @@ const aut0Login = async (req, res) => {
 
     return res.status(200).json({
       ok: true,
-      user: { id: user._id, name: user.name, email: user.email },
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        img: user.img.secure_url,
+      },
     });
   } catch (err) {
     return res.status(503).json({
       ok: false,
       message: "Something happened",
-    });
-  }
-};
-
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Wrong email or password",
-      });
-    }
-
-    const userPassword = user.password;
-    const isPasswordOk = await bcrypt.compare(password, userPassword);
-
-    if (!isPasswordOk) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Wrong email or password",
-      });
-    }
-    return res.status(200).json({
-      ok: true,
-      user: user._id,
-    });
-  } catch (error) {
-    return res.status(503).json({
-      ok: false,
-      msg: "Something happened",
     });
   }
 };
@@ -146,8 +73,6 @@ const editImage = async (req, res) => {
 };
 
 module.exports = {
-  addNewUser,
-  loginUser,
   editImage,
   aut0Login,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const { addNewUser, loginUser, editImage, aut0Login } = require("../controllers/userControllers");
+const { editImage, aut0Login } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.post("/register", addNewUser);
-router.post("/login", loginUser);
 router.post("/img", editImage);
 router.post("/auth0login", aut0Login);
 
