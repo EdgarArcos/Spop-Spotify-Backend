1eeb2eceeb757265a595f1a7971b9f912a05aa95
Isaura Martí <imarti.teixido@gmail.com>
2023-05-03 10:59:51 +0200

    feat route music

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
 const { dbConnection } = require("./database/config");
-const  fileUpload = require("express-fileupload");
+const fileUpload = require("express-fileupload");
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const listRoutes = require("./routes/listRoutes");
+const musicRoutes = require("./routes/musicRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(fileUpload({
-  useTempFiles: true,
-  tempFileDir: "./uploads",
-}))
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
 
 dbConnection();
 
@@ -23,6 +26,7 @@ dbConnection();
 
 app.use("/users", userRoutes);
 app.use("/list", listRoutes);
+app.use("/music", musicRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on PORT... ${process.env.PORT || 5000}`);
