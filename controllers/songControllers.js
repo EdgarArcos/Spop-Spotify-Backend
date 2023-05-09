130e242a93adac2e2b467904ce68c89341096396
Edgar Arcos <edgararcosmartin@gmail.com>
2023-05-09 15:50:24 +0100

    Add editar cancion (titulo y genero)

--- controllers/songControllers.js
+++ controllers/songControllers.js
@@ -1,6 +1,6 @@
 const fs = require("fs-extra")
 const Song = require("../models/Song");
-const { uploadSong, deleteSongCloud, uploadImgSong } = require("../utils/cloudinary");
+const { uploadSong, deleteSongCloud, uploadImgSong, deleteImage } = require("../utils/cloudinary");
 
 
 const getSongs = async (req, res) => {
@@ -41,8 +41,10 @@ const createSong = async (req, res) => {
 const deleteSongs = async (req, res) => {
     try {
         const songRemoved = await Song.findByIdAndDelete(req.params.id)
+        console.log(songRemoved);
         if (!songRemoved) return res.sendStatus(404)
-        await deleteSongCloud(songRemoved.public_id)
+        await deleteSongCloud(songRemoved.song.public_id)
+        await deleteImage(songRemoved.image.public_id)
         return res.sendStatus(204)
     } catch (error) {
         return res.status(500).json({ message: error.message })
@@ -51,8 +53,17 @@ const deleteSongs = async (req, res) => {
 
 const updateSongs = async (req, res) => {
     try {
-        await Song.findByIdAndUpdate(req.params._id, req.body, { new: true })
+        await Song.findByIdAndUpdate(req.params.id, req.body, { new: true })
         return res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+const getSongId = async (req, res) => {
+    try {
+        const songById = await Song.findById(req.params.id)
+        if (!songById) return res.sendStatus(404)
+        return res.json(songById)
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
@@ -62,7 +73,8 @@ module.exports = {
     getSongs,
     createSong,
     deleteSongs,
-    updateSongs
+    updateSongs,
+    getSongId
 };
 
 
