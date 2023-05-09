eddc5e591bcf21f08b4373760da1290c4ee1f73f
Iñaki García <igardiet@gmail.com>
2023-05-09 16:56:23 +0200

    deleteImg moved to top

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -13,15 +13,16 @@ const uploadImg = async (filePath) => {
     });
 };
 
+const deleteImg = async (public_id) => {
+    return await cloudinary.uploader.destroy(public_id);
+};
+
 const uploadImgSong = async (filePath) => {
     return await cloudinary.uploader.upload(filePath, {
         folder: "Artist_Songs",
     });
 };
 
-const deleteImg = async (public_id) => {
-    return await cloudinary.uploader.destroy(public_id);
-};
 
 
 
