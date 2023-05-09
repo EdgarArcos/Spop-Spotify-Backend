71dc584a376fd0882015c2d295c1a22cf2f7b8d5
Isaura Martí <imarti.teixido@gmail.com>
2023-05-09 13:45:05 +0200

    feat handle liked songs playlist

--- controllers/musicControllers.js
+++ controllers/musicControllers.js
@@ -1,5 +1,6 @@
 const Song = require("../models/Song");
 const Genre = require("../models/Genre");
+const Playlist = require("../models/Playlist");
 
 const getAllGenres = async (req, res) => {
   try {
@@ -34,7 +35,42 @@ const getSongsByGenre = async (req, res) => {
   }
 };
 
+const handleLikeSong = async (req, res) => {
+  const { userId, songId } = req.body;
+
+  try {
+    const likedSongs = await Playlist.findOne({
+      user: userId,
+      title: "Liked Songs",
+    });
+
+    if (likedSongs.songs.includes(songId)) {
+      likedSongs.songs.pull(songId);
+      await likedSongs.save();
+      return res.status(200).json({
+        ok: true,
+        likedSongs,
+        msg: "deleted",
+      });
+    }
+    likedSongs.songs.push(songId);
+    await likedSongs.save();
+    return res.status(200).json({
+      ok: true,
+      likedSongs,
+      msg: "added",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(503).json({
+      ok: false,
+      msg: "Something happened",
+    });
+  }
+};
+
 module.exports = {
   getAllGenres,
   getSongsByGenre,
+  handleLikeSong,
 };
--- routes/musicRoutes.js
+++ routes/musicRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getAllGenres,
   getSongsByGenre,
+  handleLikeSong,
 } = require("../controllers/musicControllers");
 
 const router = express.Router();
 
 router.get("/genres", getAllGenres);
 router.get("/:genre", getSongsByGenre);
+router.post("/handlelikesong", handleLikeSong);
 
 module.exports = router;
